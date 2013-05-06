7752f93003d25edd8372cd65d42b43f4e111e5f7
Conor O'Callaghan <conor_oc@ymail.com>
2013-05-06 02:13:12 +0100

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Codequery::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  config.assets.enabled = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
